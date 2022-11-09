0e97e20f217490d9cd5f615581470f2fcc43f66c
Daffa Kurnia Fatah <72376750+daffakurnia11@users.noreply.github.com>
2022-11-09 23:15:24 +0700

    Adding comment

--- main.py
+++ main.py
@@ -26,25 +26,25 @@ while True:
         if i == 1:
             cmd[2] = 0
             cmd[3] = i
-            cmd[4] = 0xFF
+            cmd[4] = 0xFF  # Normally Closed
             cmd[5] = 0
             crc = pycrc.ModbusCRC(cmd[0:6])
             cmd[6] = crc & 0xFF
             cmd[7] = crc >> 8
             print(cmd)
             s.write(cmd)
-            time.sleep(1)
+            time.sleep(1)  # Program Delay
         if i == 2:
             cmd[2] = 0
             cmd[3] = i
-            cmd[4] = 0xFF
+            cmd[4] = 0xFF  # Normally Closed
             cmd[5] = 0
             crc = pycrc.ModbusCRC(cmd[0:6])
             cmd[6] = crc & 0xFF
             cmd[7] = crc >> 8
             print(cmd)
             s.write(cmd)
-            time.sleep(1)
+            time.sleep(1)  # Program Delay
 
         # VARIASI 2 - DAFFA
         # if i == 0:
