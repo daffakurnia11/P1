4e7846c4e62841d6729a2e6109fb4660b3d09963
Daffa Kurnia Fatah <72376750+daffakurnia11@users.noreply.github.com>
2022-11-09 23:12:11 +0700

    Adding comment

--- main.py
+++ main.py
@@ -1,29 +1,28 @@
-#!/usr/bin/env python3
-# -*- coding:utf-8 -*-
 import serial
 import pycrc
 import time
 
-s = serial.Serial("COM10", 9600)
+s = serial.Serial("COM7", 9600)
 cmd = [0, 0, 0, 0, 0, 0, 0, 0]
 
-cmd[0] = 0x01
-cmd[1] = 0x05
+cmd[0] = 0x01  # Modbus Address
+cmd[1] = 0x05  # Write single Relay
 
 while True:
     for i in range(8):
-        # VARIASI 1 - DAFFA
+        # First Variation
+        # Channel 1, 2, 3
         if i == 0:
             cmd[2] = 0
             cmd[3] = i
-            cmd[4] = 0xFF
+            cmd[4] = 0xFF  # Normally Closed
             cmd[5] = 0
             crc = pycrc.ModbusCRC(cmd[0:6])
             cmd[6] = crc & 0xFF
             cmd[7] = crc >> 8
             print(cmd)
             s.write(cmd)
-            time.sleep(1)
+            time.sleep(1)  # Program Delay
         if i == 1:
             cmd[2] = 0
             cmd[3] = i
--- open_close_relay.py
+++ open_close_relay.py
@@ -1,24 +1,22 @@
-#!/usr/bin/env python3
-# -*- coding:utf-8 -*-
 import serial
 import pycrc
 import time
 
-s = serial.Serial("COM5", 9600)
+s = serial.Serial("COM7", 9600)
 cmd = [0, 0, 0, 0, 0, 0, 0, 0]
 
-cmd[0] = 0x01
-cmd[1] = 0x05
+cmd[0] = 0x01  # Modbus Address
+cmd[1] = 0x05  # Write single Relay
 
 while True:
     for i in range(8):
         cmd[2] = 0
         cmd[3] = i
-        cmd[4] = 0
+        cmd[4] = 0  # Normally Open
         cmd[5] = 0
         crc = pycrc.ModbusCRC(cmd[0:6])
         cmd[6] = crc & 0xFF
         cmd[7] = crc >> 8
         print(cmd)
         s.write(cmd)
-        time.sleep(0.2)
+        time.sleep(0.2)  # Program Delay
